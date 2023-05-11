b81d84ca8fa59b6d19d76c8d8a1a76d869265882
Stefan Schätz <stefan.schaetz@codecentric.de>
2023-05-11 20:20:34 +0200

    Finish param-helper tests

--- test/param-helper.spec.ts
+++ test/param-helper.spec.ts
@@ -1,12 +1,39 @@
 import {describe, expect, test} from '@jest/globals';
 import {getCountQueryParameter} from "../lib/param-helper";
 import {Request} from "express";
+import {InvalidUserParameterError} from "../lib/types";
 
 describe('param-helper', () => {
-    test('getCountQueryParameter should return 7 if the count parameter is 7', () => {
+    test('getCountQueryParameter should return the default search count if no count parameter is given', () => {
+        const mockRequest = {query: {}} as any as Request;
+        expect(getCountQueryParameter(mockRequest)).toEqual(10);
+    });
+
+    test('getCountQueryParameter should return the given count parameter if it is a valid number', () => {
         const mockRequest = _createMockRequest('7');
         expect(getCountQueryParameter(mockRequest)).toEqual(7);
-    })
+    });
+
+    test('getCountQueryParameter should return the maximum search count if the count parameter is bigger', () => {
+        const mockRequest = _createMockRequest('31');
+        expect(getCountQueryParameter(mockRequest)).toEqual(30);
+    });
+
+    test('getCountQueryParameter should throw an error if the count parameter is not a number', () => {
+        const requestWrapper = () => {
+            const mockRequest = _createMockRequest('abcdef');
+            getCountQueryParameter(mockRequest)
+        };
+        expect(requestWrapper).toThrow(InvalidUserParameterError);
+    });
+
+    test('getCountQueryParameter should throw an error if the count parameter is 0', () => {
+        const requestWrapper = () => {
+            const mockRequest = _createMockRequest('0');
+            getCountQueryParameter(mockRequest)
+        };
+        expect(requestWrapper).toThrow(InvalidUserParameterError);
+    });
 
     const _createMockRequest = (count: string) => {
         return {
